688887259d4bae94832fa13a330fe3f449d4eac1
Rohit <63205012+rohittechzone@users.noreply.github.com>
2020-06-21 18:24:36 +0530

    Add files via upload

--- Ink.js
+++ Ink.js
@@ -4,6 +4,7 @@ class Ink {
           isStatic: true
       }
       this.ink = [];
+      this.inkImage = loadImage("ink.png");
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
@@ -14,5 +15,16 @@ class Ink {
       rectMode(CENTER);
       fill("brown");
       rect(pos.x, pos.y, this.width, this.height);
+      if(inkstate === 1){
+        var position = [mouseX, mouseY];
+        this.ink.push(position);
+      }
+     
+  
+      for(var i=0; i<this.ink.length; i++){
+        image(this.inkImage, this.ink[i][0], this.ink[i][1]);
+      }
     }
   };
+    
+  
--- sketch.js
+++ sketch.js
@@ -6,18 +6,18 @@ const Constraint = Matter.Constraint;
 var inkstate = 0;
 var engine, world;
 var position; 
-var ink = [];
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+inkreal = new Ink(1333,744,1,1);
    //ink = new Ink(200,200,50,50);
 }
 
 function draw(){
-    background(255,255,255);
+    background("pink");
     Engine.update(engine);
+    inkreal.display();
    /* if(inkstate === 1){
         var position = [mouseX,mouseY];
         ink.push(position);
@@ -25,9 +25,7 @@ function draw(){
     for(var i=0; i<ink.length; i++){
          rect(ink[i][0],ink[0][i],10,10);
     }*/
-    if(inkstate === 1){
-        rect(mouseX,mouseY,5,5);
-       }
+    
 }
 function mouseDragged(){
     inkstate = 1;
